48411b2823fa12fa9f877de0775e0fcc0dc24a44
alexsoliveira <alexsoliveira@gmail.com>
2025-05-14 15:39:10 -0300

    Recuperando o Encode do código de Barras

--- fnGeradorBoletos/fnGeradorBoletos/Function1.cs
+++ fnGeradorBoletos/fnGeradorBoletos/Function1.cs
@@ -1,4 +1,5 @@
 using Azure.Messaging.ServiceBus;
+using BarcodeStandard;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -17,7 +18,7 @@ namespace fnGeradorBoletos
         public GeradorCodigoBarras(ILogger<GeradorCodigoBarras> logger)
         {
             _logger = logger;
-            _serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString")
+            _serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
         }
 
         [Function("barcode-generate")]
@@ -25,24 +26,70 @@ namespace fnGeradorBoletos
         {
             try
             {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
 
+                string valor = data?.valor;
+                string dataVencimento = data?.dataVencimento;
 
-                var resultObject = new
+                string barcodeData;
+
+                // Validação dos dados
+                if (string.IsNullOrEmpty(valor) || string.IsNullOrEmpty(dataVencimento))
                 {
-                    barcode = "1234567890",
-                    valorOriginal = 100.00,
-                    DataVencimento = DateTime.Now.AddDays(5),
-                    ImagemBase64 = "ImagemBase64"
-                };
+                    return new BadRequestObjectResult("Os campos valor e daVencimento são obrigatórios");
+                }
 
-                await SendFileFallback(resultObject, _serviceBusConnectionString, _queueName);
+                // Validar formato da Data de Vencimento YYYY-MM-DD
+                if(!DateTime.TryParseExact(dataVencimento, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime dateObj))
+                {
+                    return new BadRequestObjectResult("Data de Vencimento inválida");
+                }
+
+                string dateStr = dateObj.ToString("yyyyMMdd");
+
+                // Conversão do Valor para centavos e formatação até 8 digitos
+                if (!decimal.TryParse(valor, out decimal valorDecimal))
+                {
+                    return new BadRequestObjectResult("Valor inválido");
+                }
+
+                int valorCentavos = (int)(valorDecimal * 10);
+                string valorStr = valorCentavos.ToString("D8");
+
+                string bank = "008";
+                string baseCode = string.Concat(bank, dateStr, valorStr);
+
+                // Preenchimento do barcode para ter 44 caracteres
+                barcodeData = baseCode.Length < 44 ? baseCode.PadRight(44, '0') : baseCode.Substring(0, 44);
+                _logger.LogInformation($"Barcode gerado: {barcodeData}");
+
+                Barcode barcode = new Barcode();
+                var skImage = barcode.Encode(BarcodeStandard.Type.Code128, barcodeData);
+
+                using (var encodeData = skImage.Encode(SkiaSharp.SKEncodedImageFormat.Png, 100))
+                {
+                    var imageBytes = encodeData.ToArray();
+
+                    string base64String = Convert.ToBase64String(imageBytes);
+
+                    var resultObject = new
+                    {
+                        barcode = barcodeData,
+                        valorOriginal = valorDecimal,
+                        DataVencimento = DateTime.Now.AddDays(5),
+                        ImagemBase64 = base64String
+                    };
+
+                    await SendFileFallback(resultObject, _serviceBusConnectionString, _queueName);
+
+                    return new OkObjectResult(resultObject);
+                }                                
             }
             catch (Exception ex)
             {
-                throw;
-            }
-
-            return new OkObjectResult("Welcome to Azure Functions!");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }            
         }
 
         private async Task SendFileFallback(object resultObject, string serviceBusConnectionString, string queueName)
