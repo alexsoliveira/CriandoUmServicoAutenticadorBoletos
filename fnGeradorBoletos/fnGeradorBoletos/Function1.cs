cf16c18691e517e963e72ea1d15bb542dd225db5
alexsoliveira <alexsoliveira@gmail.com>
2025-05-14 14:47:07 -0300

    Criando namespace e configurando o lab

--- fnGeradorBoletos/fnGeradorBoletos/Function1.cs
+++ fnGeradorBoletos/fnGeradorBoletos/Function1.cs
@@ -1,24 +1,63 @@
+using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace fnGeradorBoletos
 {
-    public class Function1
+    public class GeradorCodigoBarras
     {
-        private readonly ILogger<Function1> _logger;
+        private readonly ILogger<GeradorCodigoBarras> _logger;
+        private readonly string _serviceBusConnectionString;
+        private readonly string _queueName = "gerador-codigo-barras";
 
-        public Function1(ILogger<Function1> logger)
+        public GeradorCodigoBarras(ILogger<GeradorCodigoBarras> logger)
         {
             _logger = logger;
+            _serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString")
         }
 
-        [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        [Function("barcode-generate")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+
+
+                var resultObject = new
+                {
+                    barcode = "1234567890",
+                    valorOriginal = 100.00,
+                    DataVencimento = DateTime.Now.AddDays(5),
+                    ImagemBase64 = "ImagemBase64"
+                };
+
+                await SendFileFallback(resultObject, _serviceBusConnectionString, _queueName);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
             return new OkObjectResult("Welcome to Azure Functions!");
+        }
+
+        private async Task SendFileFallback(object resultObject, string serviceBusConnectionString, string queueName)
+        {
+            await using var client = new ServiceBusClient(serviceBusConnectionString);
+
+            ServiceBusSender sender = client.CreateSender(queueName);
+
+            string messageBody = JsonConvert.SerializeObject(resultObject);
+
+            ServiceBusMessage message = new ServiceBusMessage(messageBody);
+
+            await sender.SendMessageAsync(message);
+
+            _logger.LogInformation($"Mensagem eniada para a fila {queueName}");
         }
     }
 }
